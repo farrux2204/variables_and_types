032febb860804a84068a6a49c249d3fbc71385d8
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-07 13:18:45 +0500

    8

--- begin08.py
+++ begin08.py
@@ -1,2 +1,6 @@
 # Quyidagi README faylda masala sharti berilgan
-# https://github.com/codeschool-2022A/variables_and_types#begin08
+# https://github.com/codeschool-2022A/
+a=5
+b=4
+c=8
+print(a*b/c)
